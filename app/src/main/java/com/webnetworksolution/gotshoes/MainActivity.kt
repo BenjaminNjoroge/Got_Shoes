3ac58d447638433e0d1cfff55bfe4c07d94fccd3
Benja <benjambuthia@gmail.com>
2023-02-07 08:47:47 +0300

    starter-files

--- app/src/main/java/com/webnetworksolution/gotshoes/MainActivity.kt
+++ app/src/main/java/com/webnetworksolution/gotshoes/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is just a test commit")
+
+        println("Experimental")
     }
 }
